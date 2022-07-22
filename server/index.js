de13ff086871bdb927f4f08de7b42fa908f504f8
vivian <vivian.v18723@gmail.com>
2022-07-22 10:52:44 +0100

    and get & listen methods

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
-app = require("express");
+const importedExpressModule = require("express");
+const instanceOfExpress = importedExpressModule();
+const localHostPort = 3000;
+const allFurniture = require("./furnitureData");
 
-app.get("/", (req, res) => {
-  res.send("Welcome to furniture API!");
+// uses get method
+instanceOfExpress.get("/", (req, res) => {
+  res.send("Welcome to the furniture API!");
 });
+
+instanceOfExpress.get("/fullFurnitureArr", (req, res) => {
+  res.send(allFurniture);
+});
+
+instanceOfExpress.listen(
+  localHostPort,
+  console.log(`listening on port 3000 ;) ...`)
+);
