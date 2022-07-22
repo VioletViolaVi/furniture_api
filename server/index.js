417dd0002abe04efc92a2d942e221f7c8e314e6a
vivian <vivian.v18723@gmail.com>
2022-07-22 11:50:03 +0100

    create get methods to display specific arr of objs

--- server/index.js
+++ server/index.js
@@ -5,11 +5,50 @@ const allFurniture = require("./furnitureData");
 
 // uses get method
 instanceOfExpress.get("/", (req, res) => {
-  res.send("Welcome to the furniture API!");
+  res.send("Welcome to the server furniture API!");
 });
 
 instanceOfExpress.get("/fullFurnitureArr", (req, res) => {
   res.send(allFurniture);
+});
+
+// gets only kitchen furniture
+instanceOfExpress.get("/kitchenItems", (req, res) => {
+  let kitchenFurniture = [];
+
+  allFurniture.forEach((singleFurnitureObj) => {
+    if (singleFurnitureObj.room === "kitchen".toLowerCase()) {
+      kitchenFurniture.push(singleFurnitureObj);
+    }
+  });
+
+  res.send(kitchenFurniture);
+});
+
+// gets only bedroom furniture
+instanceOfExpress.get("/bedroomItems", (req, res) => {
+  let bedroomFurniture = [];
+
+  allFurniture.forEach((singleFurnitureObj) => {
+    if (singleFurnitureObj.room === "bedroom".toLowerCase()) {
+      bedroomFurniture.push(singleFurnitureObj);
+    }
+  });
+
+  res.send(bedroomFurniture);
+});
+
+// gets only bathroom furniture
+instanceOfExpress.get("/bathroomItems", (req, res) => {
+  let bathroomFurniture = [];
+
+  allFurniture.forEach((singleFurnitureObj) => {
+    if (singleFurnitureObj.room === "bathroom".toLowerCase()) {
+      bathroomFurniture.push(singleFurnitureObj);
+    }
+  });
+
+  res.send(bathroomFurniture);
 });
 
 instanceOfExpress.listen(
