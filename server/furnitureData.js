b787c95fd60337097f1b18e3180b244a36ac45d2
vivian <vivian.v18723@gmail.com>
2022-07-22 10:52:23 +0100

    change file name

---
+++ server/furnitureData.js
@@ -0,0 +1,13 @@
+const furnitureArr = [
+  { id: 1, furnitureName: "fridge", room: "kitchen" },
+  { id: 2, furnitureName: "oven", room: "kitchen" },
+  { id: 3, furnitureName: "freezer", room: "kitchen" },
+  { id: 4, furnitureName: "bed", room: "bedroom" },
+  { id: 5, furnitureName: "alarm clock", room: "bedroom" },
+  { id: 6, furnitureName: "wardrobe", room: "bedroom" },
+  { id: 7, furnitureName: "shower", room: "bathroom" },
+  { id: 8, furnitureName: "bathtub", room: "bathroom" },
+  { id: 9, furnitureName: "toilet", room: "bathroom" },
+];
+
+module.exports = furnitureArr;
